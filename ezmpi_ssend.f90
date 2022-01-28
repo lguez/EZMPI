008ec33e2d634846a3025872672352817e7f21f9
Lionel GUEZ <guez@lmd.ens.fr>
2022-01-28 15:53:04 +0100

    Rename interface to `ezmpi_ssend`

--- ezmpi_ssend.f90
+++ ezmpi_ssend.f90
@@ -1,22 +1,22 @@
-module ezmpi_send_m
+module ezmpi_ssend_m
 
   use mpi_f08, only: mpi_ssend, mpi_real, mpi_logical, mpi_comm_world, &
        mpi_integer
 
   implicit none
 
-  interface ezmpi_send
-     module procedure ezmpi_send_0d_real, ezmpi_send_1d_real, &
-          ezmpi_send_2d_real, ezmpi_send_0d_logical, ezmpi_send_0d_integer, &
-          ezmpi_send_2d_integer
-  end interface ezmpi_send
+  interface ezmpi_ssend
+     module procedure ezmpi_ssend_0d_real, ezmpi_ssend_1d_real, &
+          ezmpi_ssend_2d_real, ezmpi_ssend_0d_logical, ezmpi_ssend_0d_integer, &
+          ezmpi_ssend_2d_integer
+  end interface ezmpi_ssend
 
   private
-  public ezmpi_send
+  public ezmpi_ssend
 
 contains
 
-  subroutine ezmpi_send_0d_real(buf, dest, tag)
+  subroutine ezmpi_ssend_0d_real(buf, dest, tag)
 
     real, intent(in):: buf
     integer, intent(in):: dest, tag
@@ -25,11 +25,11 @@ contains
 
     call mpi_ssend(buf, 1, mpi_real, dest, tag, mpi_comm_world)
 
-  end subroutine ezmpi_send_0d_real
+  end subroutine ezmpi_ssend_0d_real
 
   !****************************************************
 
-  subroutine ezmpi_send_1d_real(buf, dest, tag)
+  subroutine ezmpi_ssend_1d_real(buf, dest, tag)
 
     real, intent(in):: buf(:)
     integer, intent(in):: dest, tag
@@ -39,11 +39,11 @@ contains
     if (size(buf) /= 0) call mpi_ssend(buf(1), size(buf), mpi_real, dest, tag, &
          mpi_comm_world)
 
-  end subroutine ezmpi_send_1d_real
+  end subroutine ezmpi_ssend_1d_real
 
   !****************************************************
 
-  subroutine ezmpi_send_2d_real(buf, dest, tag)
+  subroutine ezmpi_ssend_2d_real(buf, dest, tag)
 
     real, intent(in):: buf(:, :)
     integer, intent(in):: dest, tag
@@ -53,11 +53,11 @@ contains
     if (size(buf) /= 0) call mpi_ssend(buf(1, 1), size(buf), mpi_real, dest, &
          tag, mpi_comm_world)
 
-  end subroutine ezmpi_send_2d_real
+  end subroutine ezmpi_ssend_2d_real
 
   !****************************************************
 
-  subroutine ezmpi_send_0d_logical(buf, dest, tag)
+  subroutine ezmpi_ssend_0d_logical(buf, dest, tag)
 
     logical, intent(in):: buf
     integer, intent(in):: dest, tag
@@ -66,11 +66,11 @@ contains
 
     call mpi_ssend(buf, 1, mpi_logical, dest, tag, mpi_comm_world)
 
-  end subroutine ezmpi_send_0d_logical
+  end subroutine ezmpi_ssend_0d_logical
 
   !****************************************************
 
-  subroutine ezmpi_send_0d_integer(buf, dest, tag)
+  subroutine ezmpi_ssend_0d_integer(buf, dest, tag)
 
     integer, intent(in):: buf
     integer, intent(in):: dest, tag
@@ -79,11 +79,11 @@ contains
 
     call mpi_ssend(buf, 1, mpi_integer, dest, tag, mpi_comm_world)
 
-  end subroutine ezmpi_send_0d_integer
+  end subroutine ezmpi_ssend_0d_integer
 
   !****************************************************
 
-  subroutine ezmpi_send_2d_integer(buf, dest, tag)
+  subroutine ezmpi_ssend_2d_integer(buf, dest, tag)
 
     integer, intent(in):: buf(:, :)
     integer, intent(in):: dest, tag
@@ -93,6 +93,6 @@ contains
     if (size(buf) /= 0) call mpi_ssend(buf(1, 1), size(buf), mpi_integer, &
          dest, tag, mpi_comm_world)
 
-  end subroutine ezmpi_send_2d_integer
+  end subroutine ezmpi_ssend_2d_integer
 
-end module ezmpi_send_m
+end module ezmpi_ssend_m
